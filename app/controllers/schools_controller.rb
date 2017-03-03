708b278c8b725b5522eba2ce58d86cda8b1cfcf2
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-03-02 19:18:08 -0800

    recover lost data, reset database, add if statements to controller and schools#index to test if @schools?

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -10,9 +10,11 @@ class SchoolsController < ApplicationController
       marker.lng school.longitude
       marker.infowindow school.description
     end
-    @last_school = School.last
-    @client = GooglePlaces::Client.new('AIzaSyCXYtb08IbxlM7vs60KJgJjhlEJM6xAuEg')
-    @spots = @client.spots(@last_school.latitude, @last_school.longitude, :types => 'school')
+    if @schools.any?
+      @last_school = School.last
+      @client = GooglePlaces::Client.new('AIzaSyCXYtb08IbxlM7vs60KJgJjhlEJM6xAuEg')
+      @spots = @client.spots(@last_school.latitude, @last_school.longitude, :types => 'school')
+    end
   end
 
   # GET /schools/1
--- db/migrate/20170302230425_drop_maps_school_users.rb
+++
@@ -1,6 +0,0 @@
-class DropMapsSchoolUsers < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :maps
-    drop_table :schools_users
-  end
-end
