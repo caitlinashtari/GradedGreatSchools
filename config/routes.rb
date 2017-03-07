c2bbb2613a727655ba6d11083fb5782dde5a212b
caitlin ashtari <caitlinashtari@gmail.com>
2017-03-07 11:23:56 -0800

    add html5 up template with full functionality for each view.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
-  root 'searches#index'
+  root 'home#index'
 
   resources :searches, :except => [:new, :show, :edit, :update, :destroy]
 
