80205a0ba7c9993588a7a960cfce25f60b598c70
caitlin ashtari <caitlinashtari@gmail.com>
2017-03-07 19:01:37 -0800

    resolve jquery ujs issue aka logout issue.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
-  has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   root 'home#index'
 
   resources :searches, :except => [:new, :show, :edit, :update, :destroy]
-
-  resources :users do
-    resources :reviews, :only => [:index, :show]
-  end
 
   resources :schools, :except => [:index, :new, :edit, :update, :destroy] do
     resources :reviews, :except => [:index, :show]
